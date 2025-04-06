66e393bcbdcf03d841d388b2267445d89dd4d4e8
maximumBG <60585969+maximumBG@users.noreply.github.com>
2025-04-06 18:18:53 +0300

    bug fix

--- admin/settings.php
+++ admin/settings.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../includes/auth.php';
 require_once __DIR__ . '/../includes/db.php';
+require_once __DIR__ . '/../includes/functions.php';
 
 redirectIfNotLoggedIn();
 
